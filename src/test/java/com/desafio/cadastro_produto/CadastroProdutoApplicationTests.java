6ec02841b34be86543662a5da9273df114fc3a78
Mayara <mayaradepaula1997>
2024-06-26 14:35:10 -0300

    first commit

---
+++ src/test/java/com/desafio/cadastro_produto/CadastroProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.cadastro_produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
